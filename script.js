fd9230e0ba5e630acdd0d8cd0bd6f12e94601d24
Dilsan Basnet <officialdilsanacc@gmail.com>
2026-05-06 07:39:50 +0545

    finally js done , now turn to upgrade Css

--- script.js
+++ script.js
@@ -1,19 +1,11 @@
 const units = {
-    length: { 
-        Meters: 1, Kilometers: 1000, Feet: 0.3048, Miles: 1609.34, Inches: 0.0254
-
-    },
-    weight: {
-        Kilograms: 1, Grams: 0.001, Pounds: 0.4535, Ounces: 0.0283
-    },
-    temp: {
-        Celsius: 'c', Fahrenheit: 'f', Kelvin: 'k'
-    }
+    length: { Meters: 1, Kilometers: 1000, Feet: 0.3048, Miles: 1609.34, Inches: 0.0254},
+    weight: {Kilograms: 1, Grams: 0.001, Pounds: 0.4535, Ounces: 0.0283},
+    temp: {Celsius: 'c', Fahrenheit: 'f', Kelvin: 'k' }
 };
 
 const typeEl = document.getElementById('type');;
 const fromEl = document.getElementById('fromUnit');
-;
 const toEl = document.getElementById('toUnit');
 const inputEl = document.getElementById('inputNum');
 const outputEl = document.getElementById('outputNum');
@@ -21,12 +13,12 @@ const outputEl = document.getElementById('outputNum');
 function updateOptions() {
     const category = typeEl.value;
     const options = Object.keys(units[category]);
-const optionsHTML = options.map(u `<option value="${u}">{u}</option>`).join('');;
+    
+const optionsHTML = options.map(u => `<option value="${u}">${u}</option>`).join('');
 fromEl.innerHTML = optionsHTML;
-
 toEl.innerHTML = optionsHTML;
 
-if(options.length>1) toEl.selectedIndex =1;
+if(options.length > 1) toEl.selectedIndex = 1;
 convert();
 }
 
@@ -35,19 +27,39 @@ function convert() {
     const val = parseFloat(inputEl.value);
 
     if(isNaN(val)) {
-        outputEl.value = '';
+        outputEl.value = "";
         return;
     }
-    if(cat === 'temp') {
+    if( cat === 'temp') {
         let c;
         const from = fromEl.value;
         const to = toEl.value;
 
         if(from === 'Celsius') c = val;
-        else if(from === 'Fahrenheit') c = (val -32) * 5/9;
+        else if(from === 'Fahrenheit') c = (val - 32) * 5/9;
         else c = val - 273.15;
 
-        
-    }
+
+    let res;
+    if(to === 'Celsius') res = c;
+    else if(to === 'Fahrenheit' ) res = (c * 9/5) + 32;
+    else res = c + 273.15;
+
+    outputEl.value = Number(res.toFixed(4));
+} 
+else {
+    const fromFactor = units[cat][fromEl.value];
+    const toFactor = units[cat][toEl.value];
+    const result = val*(fromFactor / toFactor);
+
+    outputEl.value = result % 1 === 0 ? result: Number(result.toFixed(5));
+
 }
+}
+typeEl.addEventListener('change', updateOptions);
+[fromEl, toEl, inputEl].forEach(el => {
+    el.addEventListener('input', convert);
+});
+
+updateOptions();
 
